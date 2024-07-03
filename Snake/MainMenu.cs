79bc568b0b568e7cb2d24e4efb8a8dfb09f9a60a
Alexander Gliese <alexander.gliese@web.de>
2024-07-03 19:46:58 +0200

    Main Menu and Game basic Frame behaviour

---
+++ Snake/MainMenu.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Snake
+{
+    public partial class MainMenu : Form
+    {
+        Game game;
+
+        public MainMenu()
+        {
+            InitializeComponent();
+            game = new Game();
+        }
+
+        private void btnStartGame_Click(object sender, EventArgs e)
+        {
+            game = new Game();
+            game.FormClosing += delegate { Application.Exit(); };
+            game.Show();
+            this.Hide();
+        }
+
+        private void MainMenu_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MainMenu_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
--- Snake/obj/Debug/net8.0-windows/Snake.AssemblyInfo.cs
+++ Snake/obj/Debug/net8.0-windows/Snake.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Snake")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12a2d357a7b6096fe3327e16bd1384889ac8cba2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Snake")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Snake")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
